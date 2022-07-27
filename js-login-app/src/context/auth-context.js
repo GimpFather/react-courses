44547251c25893eb351e8c6a5706239f1fc17d04
Filip Gałczyk <filip.galczyk@waynet.pl>
2022-07-27 17:48:54 +0200

    using context for app as provider

--- js-login-app/src/context/auth-context.js
+++ js-login-app/src/context/auth-context.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
 	isLoggedIn: false,
+	onLogout: () => {},
+	onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	useEffect(() => {
+		const cacheIsLoggedIn = localStorage.getItem("cacheIsLoggedIn");
+		if (cacheIsLoggedIn === "1") {
+			setIsLoggedIn(true);
+		}
+	}, []);
+
+	const logoutHandler = () => {
+		localStorage.removeItem("cacheIsLoggedIn");
+		setIsLoggedIn(false);
+	};
+
+	const loginHandler = () => {
+		localStorage.setItem("cacheIsLoggedIn", "1");
+		setIsLoggedIn(true);
+	};
+
+	return (
+		<AuthContext.Provider
+			value={{
+				isLoggedIn: isLoggedIn,
+				onLogout: logoutHandler,
+				onLogin: loginHandler,
+			}}
+		>
+			{props.children}
+		</AuthContext.Provider>
+	);
+};
 
 export default AuthContext;
