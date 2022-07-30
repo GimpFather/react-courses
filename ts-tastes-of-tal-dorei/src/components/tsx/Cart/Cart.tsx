44ba8c0c38db3aed6b9a2c80510e3693676f62ac
GimpFather <filip.galczyk13@gmail.com>
2022-07-30 15:36:52 +0200

    Cart little issues with styles

--- ts-tastes-of-tal-dorei/src/components/tsx/Cart/Cart.tsx
+++ ts-tastes-of-tal-dorei/src/components/tsx/Cart/Cart.tsx
@@ -3,10 +3,9 @@ import Modal from "../UI/Modal";
 const Cart = () => {
 	const cartItems = (
 		<ul className="cart-items">
-			{[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+			{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
 				<li>{item.name}</li>
 			))}
-			;
 		</ul>
 	);
 
