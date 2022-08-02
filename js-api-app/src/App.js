7a375fdd2d795a51119e6cfa2ea9843839647c8c
GimpFather <filip.galczyk13@gmail.com>
2022-08-02 20:55:49 +0200

    async await refactor

--- js-api-app/src/App.js
+++ js-api-app/src/App.js
@@ -6,22 +6,18 @@ import "./App.css";
 function App() {
 	const [movies, setMovies] = useState([]);
 
-	const fetchMoviesHandler = () => {
-		fetch("https://swapi.dev/api/films")
-			.then((repsonse) => {
-				return repsonse.json();
-			})
-			.then((data) => {
-				const transformedMovies = data.results.map((movie) => {
-					return {
-						id: movie.episode_id,
-						title: movie.title,
-						openingText: movie.opening_crawl,
-						releaseDate: movie.release_date,
-					};
-				});
-				setMovies(transformedMovies);
-			});
+	const fetchMoviesHandler = async () => {
+		const response = await fetch("https://swapi.dev/api/films");
+		const data = await response.json();
+		const transformedMovies = data.results.map((movie) => {
+			return {
+				id: movie.episode_id,
+				title: movie.title,
+				openingText: movie.opening_crawl,
+				releaseDate: movie.release_date,
+			};
+		});
+		setMovies(transformedMovies);
 	};
 
 	return (
